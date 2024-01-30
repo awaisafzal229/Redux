aa3cf83bda5f6134815e963e6dd40c1226d4d635
Awais Afzal <129833448+awaisafzal229@users.noreply.github.com>
2024-01-30 16:23:48 +0500

    Initial commit

---
+++ first-class/app/page.tsx
@@ -0,0 +1,10 @@
+import AddUsers from "./(components)/Addusers";
+import Displayusers from "./(components)/Displayusers";
+export default function Home() {
+  return (
+    <div>
+      <AddUsers/>
+      <Displayusers/>
+    </div>
+  );
+}
